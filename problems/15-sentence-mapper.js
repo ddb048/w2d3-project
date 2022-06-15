17d698c74e04cfc457699a8dbcfd9d6c3b23897a
Jesse Burnette <tubbyburnette@gmail.com>
2022-06-15 17:41:35 -0400

    problem 15

--- problems/15-sentence-mapper.js
+++ problems/15-sentence-mapper.js
@@ -25,8 +25,13 @@ let result2 = sentenceMapper("this is pretty cool right", removeVowels);
 console.log(result2); // 'ths s prtty cl rght'
 *******************************************************************************/
 
-let sentenceMapper = function() {
-
+let sentenceMapper = function(sentence, cb) {
+    let newSent = sentence.split(' ')
+    let newArray = []
+    for(let word of newSent){
+        newArray.push(cb(word))
+    }
+    return newArray.join(' ')
 };
 
 
